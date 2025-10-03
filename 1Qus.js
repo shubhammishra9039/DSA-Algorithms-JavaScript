7af3bedc83b6d757c3e635acd18c473964d0369b
shubhammishra9039 <shubhammishra6595@gmail.com>
2025-10-03 10:48:38 +0530

    update Qus1

---
+++ 1Qus.js
@@ -0,0 +1,22 @@
+// 1. Reverse an array.
+
+
+// Input:
+// const arr = [1, 2, 3, 4, 5];
+// Output:
+// [5, 4, 3, 2, 1]
+
+
+const arr = [1, 2, 3, 4, 5];
+
+let brr= []
+
+let teamp = 0
+for ( let i=arr.length-1;i>=0;i--)
+{
+    brr[teamp] =arr[i]
+    teamp++
+}
+
+
+console.log(brr);
